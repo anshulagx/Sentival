c21bc6a869f4af082f45ea8f971c3ccea0bff982
iamyajat <myajat21@gmail.com>
2020-12-13 11:21:29 +0530

    "Added list"

--- extension/popup.js
+++ extension/popup.js
@@ -1,31 +1,43 @@
-var i = 1;
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-    document.getElementById("demo").innerHTML = "Count: "+i;
-    i++;
-    // chrome.tabs.getSelected(null, function(tab) {
-    //   d = document;
-
-    //   var f = d.createElement('form');
-    //   f.action = 'options.html';
-    //   f.method = 'post';
-    //   var i = d.createElement('input');
-    //   i.type = 'hidden';
-    //   i.name = 'url';
-    //   i.value = tab.url;
-    //   f.appendChild(i);
-    //   d.body.appendChild(f);
-    //   f.submit();
-    // });
-  }, false);
+document.addEventListener('DOMContentLoaded', function () {
+  var list = document.getElementById('evsa');
+  for (var i = 0; i <= 5; i++) {
+    var firstname = "Hi: "+i;
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(firstname));
+    list.appendChild(entry);
+  }
 }, false);
 
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    message.innerText = request.source;
-  }
-});
+
+
+// var i = 1;
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   checkPageButton.addEventListener('click', function() {
+//     document.getElementById("demo").innerHTML = "Count: "+i;
+//     i++;
+//     chrome.tabs.getSelected(null, function(tab) {
+//       d = document;
+
+//       var f = d.createElement('form');
+//       f.action = 'options.html';
+//       f.method = 'post';
+//       var i = d.createElement('input');
+//       i.type = 'hidden';
+//       i.name = 'url';
+//       i.value = tab.url;
+//       f.appendChild(i);
+//       d.body.appendChild(f);
+//       f.submit();
+//     });
+//   }, false);
+// }, false);
+
+// chrome.runtime.onMessage.addListener(function(request, sender) {
+//   if (request.action == "getSource") {
+//     message.innerText = request.source;
+//   }
+// });
 
 // function onWindowLoad() {
 
