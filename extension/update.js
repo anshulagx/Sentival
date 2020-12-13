636e9881f0dc1661ca2fb0816befb9048fa5db15
divit-agrawal <73773075+divit-agrawal@users.noreply.github.com>
2020-12-13 09:33:30 +0530

    Extension updated

--- extension/update.js
+++ extension/update.js
@@ -11,11 +11,18 @@ var paras = []
 for(var i = 0; i<x.length ; i++){
     var str = x[i].innerText;
     var result = str.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g);
+    try{
     for(var j = 0; j<result.length ; j++)
         paras.push(result[j]);
 }
+catch(err){
+    continue;
+}
+}
+
 
 x[0].innerHTML ="<b style=\"color:green;\">HELLO</b>" + paras[0];
+
 
 
 
